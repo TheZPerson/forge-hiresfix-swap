73eb8a44401b432f119a3edda2b90c73f8ce7ba0
TheZman <Zatman@DESKTOP-K24V8NP>
2026-07-20 11:20:41 +0300

    add options

--- scripts/forge_hiresfix_swap.py
+++ scripts/forge_hiresfix_swap.py
@@ -1,6 +1,6 @@
 import gradio as gr
 from torchvision.transforms.functional import _is_pil_image
-from modules import scripts
+from modules import scripts, shared, script_callbacks
 from modules.processing import StableDiffusionProcessing
 from modules.ui_components import InputAccordion
 
@@ -24,10 +24,10 @@ class Forge_HiresFix_Swap(scripts.Script):
     def ui(self, is_img2img):
           with InputAccordion(False, label=self.title()) as enable:
             if is_neo:
-                inputImage = ForgeCanvas(scribble_color="#FFFFFF", scribble_softness=25)
+                inputImage = ForgeCanvas(height = getattr(shared.opts, "hires_fix_swap_maxheight",300), scribble_color=getattr(shared.opts, "hires_fix_swap_color","#ffffff"), scribble_softness=getattr(shared.opts, "hires_fix_swap_canvasSoftness",100), scribble_width=getattr(shared.opts, "hires_fix_swap_size",25), scribble_alpha=getattr(shared.opts, "hires_fix_swap_opacity",100)  )
                 inputImage.do_not_save_to_config = True
             else:
-                inputImage = gr.Image(type="pil",label="img",show_label=False)
+                inputImage = gr.Image(height = getattr(shared.opts, "hires_fix_swap_maxheight",300),type="pil",label="img",show_label=False)
 
             if is_neo:
                 return [enable, inputImage.background ,inputImage.foreground]
@@ -60,3 +60,16 @@ class Forge_HiresFix_Swap(scripts.Script):
         merged = fg_rgb * alpha + bg_rgb * (1.0 - alpha)
 
         return np.clip(merged.round(), 0, 255).astype(np.uint8)
+
+
+def on_ui_settings():
+    section = ("hires_fix_swap", "Hires Fix Img Swapper")
+    shared.opts.add_option("hires_fix_swap_maxheight",shared.OptionInfo(default=300,label="Canvas Max Height", component=gr.Number, component_args = {'precision':0, 'minimum': 0,},section=section).needs_reload_ui())
+
+    if is_neo:
+        shared.opts.add_option("hires_fix_swap_canvasSoftness",shared.OptionInfo(default=25,label="Brush Softness", component=gr.Number, component_args = {'precision':0, 'minimum': 0, 'maximum' : 100 },section=section).needs_reload_ui())
+        shared.opts.add_option("hires_fix_swap_size",shared.OptionInfo(default=100,label="Brush Size", component=gr.Number, component_args = {'precision':0, 'minimum': 1, 'maximum' : 100 },section=section).needs_reload_ui())
+        shared.opts.add_option("hires_fix_swap_color",shared.OptionInfo(default="#ffffff",label="Brush Color",component=gr.ColorPicker,section=section).needs_reload_ui())
+        shared.opts.add_option("hires_fix_swap_opacity",shared.OptionInfo(default=100,label="Brush Opacity", component=gr.Number, component_args = {'precision':0, 'minimum': 0, 'maximum' : 100 },section=section).needs_reload_ui())
+
+script_callbacks.on_ui_settings(on_ui_settings)
